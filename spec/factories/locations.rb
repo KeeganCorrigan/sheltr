21837c819d73c94189155027bdd6a62234012e1d
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-09-09 21:48:48 -0600

    add partials to search index view

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :location do
-    name { "denver rescue mission" }
-    formatted_address { "1234 Lawrence Street, denver, co" }
-    formatted_phone_number { "303-956-8899"}
-    website { "denverrescuemission.com" }
-    open_now { True }
-    hours { "Sunday: 12:00 – 5:00 PM" }
+    name  "denver rescue mission"
+    formatted_address  "2301 Lawrence St, Denver, CO 80205, USA"
+    formatted_phone_number  "(303) 294-0241"
+    website  "denverrescuemission.com"
+    open_now  "true"
+    hours  ["4:30 PM", "8:00 AM"]
   end
 end
