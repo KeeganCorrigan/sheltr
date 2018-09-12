efa38d63a6036b0ec80f4d5b2a60a1a045f2a150
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-09-11 22:53:22 -0600

    adds languages for about page, sets locale for en and es, adds yml file for en and es

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  before_action :set_locale
 
   def current_user
     current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def set_locale
+   locale = params[:locale].to_s.strip.to_sym
+   I18n.locale = I18n.available_locales.include?(locale) ?
+       locale :
+       I18n.default_locale
+ end
 end
